23b1275801e1c5e5762fb60bcb698885290b1e2f
Hexcifer <naufalsaikyo@gmail.com>
2023-12-18 11:49:01 +0700

    mantap wak

---
+++ Ujian.php
@@ -0,0 +1,11 @@
+<?php
+<h2>Programming</h2>
+<p>Saya mengerjakan ujian ini dengan jujur!</p>
+<img src="gambar.jpg" alt="Deskripsi gambar"/>
+/"sedang mengerjakan soal!"/
+/"Dilarang berisik, ada ujian!"
+"Tetap fokus dan konsentrasi saat belajar, hindari gangguan dan periksa jadwal secara rutin./"
+
+<textarea placeholder="isi bio""></textarea>
+<p style=font-size: 50%; color: green;"Progamming</p>
+?>
